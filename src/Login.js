0fc523dd9987da2dac36fcdd8b3607c7366ca389
Alisa Yunusova <alisa.yunusova@gmail.com>
2019-05-05 11:23:00 +1200

    Make basic markup for login form

---
+++ src/Login.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function Login() {
+  return (
+    <div
+      style={{
+        textAlign: "center"
+      }}
+    >
+      <h2>Login</h2>
+      <form
+        style={{
+          display: "grid",
+          alignItems: "center",
+          justifyItems: "center"
+        }}
+      >
+        <input type="text" placeholder="Username" />
+        <input type="text" placeholder="Password" />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
