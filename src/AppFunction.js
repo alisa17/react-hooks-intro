ed8f37040167e3aa8e1d7136fc80d7225ac60870
Alisa Yunusova <alisa.yunusova@gmail.com>
2019-05-04 21:02:34 +1200

    Add latitude longitude side effect

--- src/AppFunction.js
+++ src/AppFunction.js
@@ -1,20 +1,28 @@
 import React, { useState, useEffect } from "react";
 
+const initialLocationState = {
+  latitude: null,
+  longitude: null,
+  speed: null
+};
+
 const App = () => {
   const [count, setCount] = useState(0);
   const [isOn, setIsOn] = useState(false);
   const [mousePosition, setMousePosition] = useState({ x: null, y: null });
   const [status, setStatus] = useState(navigator.onLine);
-
+  const [{latitude, longitude, speed}, setLocation] = useState(initialLocationState);
+  let mounted = true;
   // by default useEffect runs after each render
   useEffect(
     () => {
       document.title = `You have clicked ${count} times`;
-//listeners on component mount
+      //listeners on component mount
       window.addEventListener("mousemove", handleMouseMove);
       window.addEventListener("online", handleOnline);
       window.addEventListener("offline", handleOffline);
-
+      navigator.geolocation.getCurrentPosition(handleGeolocation);
+      const watchId = navigator.geolocation.watchPosition(handleGeolocation);
       // clean up function:
       return () => {
         //listeners on component Unmount
@@ -22,11 +30,23 @@ const App = () => {
         window.removeEventListener("mousemove", handleMouseMove);
         window.removeEventListener("online", handleOnline);
         window.removeEventListener("offline", handleOffline);
+        mounted = false;
+        navigator.geolocation.clearWatch(watchId);
       };
       // run useEffect only when the value in the array changes (if it was empty array it would only run on mount and unmount):
     },
     [count]
   );
+
+  const handleGeolocation = event => {
+    if (mounted) {
+      setLocation({
+        latitude: event.coords.latitude,
+        longitude: event.coords.longitude,
+        speed: event.coords.speed
+      });
+    }
+  };
 
   const handleMouseMove = event => {
     setMousePosition({ x: event.pageX, y: event.pageY });
@@ -74,6 +94,10 @@ const App = () => {
       <p>
         You are <strong>{status ? "online" : "offline"}</strong>
       </p>
+      <h2>geolocation</h2>
+      <p>Latitude is {latitude}</p>
+      <p>Longitude is {longitude}</p>
+      <p>Your speed is {speed || 0}</p>
     </>
   );
 };
